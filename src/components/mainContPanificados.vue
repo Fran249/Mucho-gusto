1c8f2e085642d60dbb3b65b0ab45ada244fda56e
Fran249 <82591984+Fran249@users.noreply.github.com>
2022-12-30 11:16:07 -0300

    30/12

--- src/components/mainContPanificados.vue
+++ src/components/mainContPanificados.vue
@@ -21,10 +21,10 @@
                     <v-card-text class="v-card-text">
                         {{card.descripcion}}
                         <p v-if="card.cantidad >= 1">
-                            {{card.cantidad}} Unidades disponibles
+                            STOCK DISPONIBLE
                         </p>
                         <p v-if="card.cantidad >= 1">${{card.precio}}</p>
-                        <p v-else>Sin stock</p>
+                        <p v-else>SIN STOCK</p>
                     </v-card-text>
                     <v-card-actions v-if="card.cantidad >= 1" class="actions-card">
                         <div class="div-btn">
