4d10919990539b6f11f662033815ccc683f41b42
Fran249 <Elsilencio_F@hotmail.com>
2023-01-02 14:52:59 -0300

    2/1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
     <!--<PreCocidos class="slider-hover"/>-->
     <!--<SucursalesVue class="sucursales-vue" v-if="!mobileViewMiddle"/>-->
     <v-btn @click="$router.push('/MuchoGustoCompras')"
-    style="margin-bottom: 205px; margin-right: 90px;"
+    style="margin-bottom: 205px; margin-right: 30px;"
       v-if="
           !mobileViewSmall"
       elevation="9"
